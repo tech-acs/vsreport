0c02a72873807a30b6bb7f69788676f0a50aa99b
tbelay <tesfayeb@gmail.com>
2024-07-14 17:01:01 +0300

    modified function to reflect comments

---
+++ R/create_table_4_3.R
@@ -0,0 +1,38 @@
+#' Creates Table 4.3
+#'
+#'Table 4.3 Live births by place of occurrence and place of usual residence of mother
+#'
+#' @param data data frame being used
+#' @param date_var occurrence data being used e.g. dobyr, dodyr etc
+#' @param data_year year the data is for
+#' @param tablename name for csv output use _ instead of . for names
+#'
+#' @return data frame with tablutated results
+#' @export
+#'
+#' @import dplyr
+#' @import tidyr
+#' @import janitor
+#'
+#' @examples t4.3 <- create_t4.3(bth_data, dobyr, 2022)
+
+create_t4.3 <- function(data, date_var, data_year = NA, tablename = "Table_4_3"){
+
+  # if data_year is not provided, take the latest year in the data
+  if (is.na(data_year)){
+    data_year = data %>% pull(!!sym(date_var)) %>% max(na.rm = TRUE)
+  }
+
+  output <- data |>
+    filter(is.na(sbind) & !!sym(date_var) == data_year) |>
+    group_by(rgnpob, usual_res_plocc) |>
+    summarise(total = n()) |>
+    pivot_wider(names_from = usual_res_plocc, values_from = total, values_fill = 0) |>
+    adorn_totals(c("col", "row"))
+
+  write.csv(output, paste0("./outputs/", tablename, ".csv"), row.names = FALSE)
+  return(output)
+}
+
+
+
