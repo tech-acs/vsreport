760d3bafd273db9b5144764599ae4c8579856465
tbelay <tesfayeb@gmail.com>
2024-07-14 20:36:18 +0300

    modified function to reflect comments

---
+++ R/create_table_4_8.R
@@ -0,0 +1,54 @@
+#' creates table 4.8
+#'
+#' Table 4.8 Crude birth rate (CBR) by place of usual residence of mother
+#'
+#' @param data data frame being used
+#' @param est_data data frame for estimate data
+#' @param date_var occurrence data variable being used
+#' @param data_year year of data
+#' @param by_var variable the data is grouped by
+#' @param tablename name for csv output use _ instead of . for names
+#'
+#' @return data frame with tabulated result
+#' @export
+#'
+#' @import dplyr
+#' @import tidyr
+#' @import janitor
+#'
+#' @examples t4.8 <- create_t4.8(bth_data, bth_est, dobyr, data_year = 2022, by_var = rgn, tablename = "Table_4_8")
+
+create_t4.8 <- function(data, est_data, date_var, data_year = NA, by_var = NA, tablename = "Table_4_8"){
+
+  # if data_year is not provided, take the latest year in the data
+  if (is.na(data_year)){
+    data_year = data %>% pull(!!sym(date_var)) %>% max(na.rm = TRUE)
+  }
+
+  output <- data |>
+  filter(!!sym(date_var) == data_year) |>
+  group_by(!!sym(by_var)) |>
+  summarise(total = n())
+
+est <- est_data |>
+  filter(year == data_year) |>
+  group_by(rgn) |>
+  summarise(est_total = sum(total))
+
+pop <- pops |>
+  select(rgn, paste0("population_", data_year)) |>
+  group_by(rgn) |>
+  summarise(total_pop = sum(!!sym(paste0("population_",data_year))))
+
+output <- left_join(output, est, by = "rgn") |>
+  mutate(completeness = round_excel(total/est_total*100, 2)) |>
+  mutate(adjusted = floor(total/(completeness/100)))
+
+output <- left_join(output, pop, by = "rgn") |>
+  mutate(cbr = round_excel(adjusted/total_pop*1000, 1)) |>
+  select(rgn, total, adjusted, cbr)
+
+write.csv(output, paste0("./outputs/", tablename, ".csv"), row.names = FALSE)
+
+return(output)
+}
