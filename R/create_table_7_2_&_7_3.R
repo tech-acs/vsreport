6c8badbbba946066b23d8490d1536a74fa049e05
Diego Lara <diego.lara.de.andres@ons.gov.uk>
2024-07-28 22:19:31 +0100

    Feat: Add use of rural_urban

--- R/create_table_7_2_&_7_3.R
+++ R/create_table_7_2_&_7_3.R
@@ -7,7 +7,7 @@
 #'
 #' @param data data frame being used
 #' @param data_year year the data is for
-#' @param ruindicator whether the data is for urban or rural
+#' @param rural_urban whether the data is for urban or rural
 #' @param tablename name of the table being saved as a csv file
 #'
 #' @return data frame of tabulated results
@@ -18,10 +18,10 @@
 #' @import janitor
 #'
 #' @examples
-#' t7.2 <- create_t7.2_and_7.3(marr_data, data_year = 2020, ruindicator = "urban", tablename = "Table_7_1")
-#' t7.3 <- create_t7.2_and_7.3(marr_data, data_year = 2020, ruindicator = "rural", tablename = "Table_7_2")
+#' t7.2 <- create_t7.2_and_7.3(marr_data, data_year = 2020, rural_urban = "urban", tablename = "Table_7_1")
+#' t7.3 <- create_t7.2_and_7.3(marr_data, data_year = 2020, rural_urban = "rural", tablename = "Table_7_2")
 #'
-create_t7.2_and_7.3 <- function(data, data_year = NA, ruindicator = "urban", tablename = NA){
+create_t7.2_and_7.3 <- function(data, data_year = NA, rural_urban = "urban", tablename = NA){
 
   # if data_year is not provided, take the latest year in the data
   if (is.na(data_year)){
@@ -30,7 +30,7 @@ create_t7.2_and_7.3 <- function(data, data_year = NA, ruindicator = "urban", tab
 
 
 output <- data |>
-  filter(year == data_year & marriage3n == "urban") |>
+  filter(year == data_year & marriage3n == rural_urban) |>
   group_by(g_age_grp, b_age_grp) |>
   summarise(total = n()) |>
   pivot_wider(names_from = b_age_grp, values_from = total, values_fill = 0) |>
