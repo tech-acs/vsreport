3cabb9e767281db3f0c30b33b9e0598d0b55849e
Diego Lara <diego.lara.de.andres@ons.gov.uk>
2024-09-15 18:16:32 +0100

    Doc: Improve function information

--- R/create_table_5_2.R
+++ R/create_table_5_2.R
@@ -1,11 +1,14 @@
 #' Creates Tables 5.2
 #'
-#' This function generates **Table 5.2**, which shows deaths by place of usual residence
+#' This function generates Table 5.2, which shows deaths by place of usual residence
 #' and the sex of the deceased.
 #'
-#' **Variables needed:**
-#' - `date_var`: Year of death (default is `"dodyr"`).
+#' Variables needed per record:
+#'
+#' - `dodyr`: Year of death.
+#'
 #' - `death2o`: Region of death.
+#'
 #' - `death2c`: Sex of the deceased.
 #'
 #' @param data Death records data frame.
@@ -34,9 +37,8 @@ create_t5.2 <- function(data, date_var = "dodyr", data_year = NA,
     summarise(total = n()) |>
     pivot_wider(names_from = death2c, values_from = total, values_fill = 0) |>
     arrange(death2o)|>
-    adorn_totals("row")|>
-    adorn_totals("col") |>
-    rename(region_of_death = death2o)
+    adorn_totals(c("col", "row"))
+    rename(region_of_residence = death2o)
 
   return(handle_table_output(output, output_path, tablename))
 }
