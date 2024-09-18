22d59d49183a2463a2f3365794e29228c0e6476b
Jeroen Minderman <Jeroen.Minderman@ons.gov.uk>
2024-09-18 10:31:49 +0100

    Fix: table 5.1

--- R/create_table_5_1.R
+++ R/create_table_5_1.R
@@ -4,6 +4,8 @@
 #' Table 5.1 Summary statistics on mortality by year of occurrence
 #'
 #' @param data deaths data frame
+#' @param est_data estimate data frame
+#' @param pops population data frame
 #' @param num_yrs number of years to report on
 #' @param tablename name for csv output use _ instead of . for names
 #'
@@ -14,26 +16,32 @@
 #' @import tidyr
 #' @import janitor
 #'
-#' @examples t5.1 <- create_t5.1(dth_data, tablename = "Table_5_1")
+#' @examples
+#' death_data <- construct_sample_death_data()
+#' estim_death_data <- construct_sample_estim_death_data()
+#' sample_pop_estim <- construct_sample_pop_estim()
+#' death_data <- construct_year(death_data, date_col = "death1a", year_col = "dodyr")
+#' table_5_1 <- create_t5.1(data = death_data, est_data = estim_death_data, pops = sample_pop_estim, tablename = "Table_5_1")
 #'
-create_t5.1 <- function(data, num_yrs=5,
+create_t5.1 <- function(data, est_data, pops, num_yrs=5,
                         tablename = "Table_5_1", output_path = NULL){
 
   max_year <- data %>% pull(dodyr) %>% max(na.rm = TRUE)
   years <- construct_year_sequence(max_year, num_yrs = num_yrs)
 
   output <- data |>
-    filter(dodyr %in% years & birth2a != "not stated")|>
-    group_by(birth2a, dodyr) |>
-    rename(Indicator = birth2a) |>
+    filter(dodyr %in% years & death2c != "not stated")|>
+    group_by(death2c, dodyr) |>
+    rename(Indicator = death2c) |>
     summarise(total = n())
 
   output_counts <- output |>
     pivot_wider(names_from = Indicator, values_from = total)
 
-  output_comp <- dth_est |>
+  output_comp <- est_data |>
     group_by(year) |>
-    summarise(ftotal = sum(female), mtotal = sum(male))
+    summarise(ftotal = sum(female), mtotal = sum(male)) |>
+    filter(year %in% years)
 
   output_comp <- cbind(output_counts, output_comp) |>
     mutate(male_comp = construct_round_excel(male/mtotal*100, 1),
@@ -52,9 +60,12 @@ create_t5.1 <- function(data, num_yrs=5,
     mutate(year = gsub("population_", "", year)) |>
     group_by(year, birth2a) |>
     summarise(total_pop = sum(count)) |>
-    arrange(birth2a)
+    arrange(birth2a, year)
 
-  output_cdr <- cbind(output, population) |>
+  output_cdr <- output |>
+    mutate(dodyr = as.character(dodyr)) |>
+    left_join(population, by = c("dodyr" = "year", "Indicator" = "birth2a")) |>
+    rename(year = dodyr) |>
     select(year, Indicator, total, total_pop ) |>
     group_by(year) |>
     summarise(total = sum(total), total_pop = sum(total_pop)) |>
